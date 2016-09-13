edcee21a27d9898e8e3ae913aa54ee7c45a8be38
nola.donato@samsung.com <nola.donato@samsung.com>
2016-09-13 13:38:29 -0700

    Adding asset loader tests

---
+++ framework-tests/app/src/main/java/org/gearvrf/tester/GVRTestableActivity.java
@@ -0,0 +1,9 @@
+package org.gearvrf.tester;
+
+import org.gearvrf.GVRActivity;
+
+public abstract class GVRTestableActivity extends GVRActivity{
+    public abstract GVRTestableMain getGVRTestableMain();
+}
+
+
