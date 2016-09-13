4bc8313ee6de699c75a06d19ac163f1318ddb7f8
nola.donato@samsung.com <nola.donato@samsung.com>
2016-09-13 13:28:53 -0700

    Adding lighting tests

---
+++ framework-tests/app/src/main/java/org/gearvrf/tester/GVRTestableMain.java
@@ -0,0 +1,9 @@
+package org.gearvrf.tester;
+
+import org.gearvrf.GVRMain;
+
+public abstract class  GVRTestableMain extends GVRMain {
+    abstract void setMainMonitor(GVRMainMonitor mainMonitor);
+    abstract boolean isOnInitCalled();
+    abstract boolean isSceneRendered();
+}
