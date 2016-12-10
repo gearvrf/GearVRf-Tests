6b7f9df9b893e1abe87f0b1581c3d7501a2462fe
nola.donato@samsung.com <nola.donato@samsung.com>
2016-12-09 18:45:27 -0800

    enable shadows, fix repeating textures

--- asset-tests/app/src/main/cpp/helpers.cpp
+++ asset-tests/app/src/main/cpp/helpers.cpp
@@ -6,11 +6,7 @@ class gvr::RenderData;
 
 JNIEXPORT jlongArray JNICALL
 Java_org_gearvrf_tester_StateSortTests_getRenderDataVector(JNIEnv *env, jclass type) {
-<<<<<<< HEAD
     const std::vector<gvr::RenderData *> &renderDataVector = gvr::gRenderer->getRenderDataVector();
-=======
-/*    const std::vector<gvr::RenderData *> &renderDataVector = gvr::gRenderer->getRenderDataVector();
->>>>>>> adding cmakelists for asset-tests
     jlongArray result = env->NewLongArray(renderDataVector.size());
 
     for (int i = 0; i < renderDataVector.size(); ++i) {
@@ -19,11 +15,6 @@ Java_org_gearvrf_tester_StateSortTests_getRenderDataVector(JNIEnv *env, jclass t
         env->SetLongArrayRegion(result, i, 1, &element);
     }
     return result;
-<<<<<<< HEAD
-=======
-    */
-    return env->NewLongArray(0);
->>>>>>> adding cmakelists for asset-tests
 }
 
 }
