ef8a717a572f2643fd6e927825c8117e75d620b1
Nola Donato <nola.donato@samsung.com>
2016-12-13 00:53:34 -0800

    fixed issues with asset event handler

--- framework-tests/app/src/androidTest/java/org/gearvrf/tester/TextureTests.java
+++ framework-tests/app/src/androidTest/java/org/gearvrf/tester/TextureTests.java
@@ -98,12 +98,11 @@ public class TextureTests
     {
         float[] texcoords = mesh.getTexCoords();
 
-        mesh.setVec2Vector("a_texcoord1", texcoords);
         for (int i = 0; i < texcoords.length; i++)
         {
             texcoords[i] *= 2.0f;
         }
-        mesh.setVec2Vector("a_texcoord", texcoords);
+        mesh.setVec2Vector("a_texcoord1", texcoords);
     }
 
     @Test
@@ -180,8 +179,13 @@ public class TextureTests
         texparams.setWrapTType(GVRTextureParameters.TextureWrapType.GL_REPEAT);
 
         GVRTexture tex1 = ctx.getAssetLoader().loadTexture(new GVRAndroidResource(ctx, R.drawable.colortex), texparams);
+        float[] texcoords = mesh.getTexCoords();
 
-        repeatTexcoords(mesh);
+        for (int i = 0; i < texcoords.length; i++)
+        {
+            texcoords[i] *= 2.0f;
+        }
+        mesh.setVec2Vector("a_texcoord", texcoords);
         mtl.setDiffuseColor(0.7f, 0.7f, 0.7f, 1);
         mtl.setSpecularColor(1, 1, 1, 1);
         mtl.setSpecularExponent(4.0f);
