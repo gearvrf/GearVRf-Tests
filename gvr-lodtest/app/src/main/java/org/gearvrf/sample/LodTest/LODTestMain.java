324ab86f0650202a10b1753941f38f088b0d1b2b
Mihail <m.marinov@samsung.com>
2017-01-10 16:20:33 -0800

    Update gvr-lodtest to use the new lod components (#41)
    * Update gvr-lodtest to use the new lod components
    
    * gvr-lodtest: add scene stats that toggle on touch

--- gvr-lodtest/app/src/main/java/org/gearvrf/sample/LodTest/LODTestMain.java
+++ gvr-lodtest/app/src/main/java/org/gearvrf/sample/LodTest/LODTestMain.java
@@ -1,36 +1,34 @@
 package org.gearvrf.sample.LodTest;
 
+import org.gearvrf.GVRAndroidResource;
+import org.gearvrf.GVRContext;
+import org.gearvrf.GVRLODGroup;
+import org.gearvrf.GVRMain;
+import org.gearvrf.GVRMaterial;
+import org.gearvrf.GVRMaterial.GVRShaderType;
+import org.gearvrf.GVRScene;
+import org.gearvrf.GVRSceneObject;
+import org.gearvrf.GVRTexture;
+import org.gearvrf.animation.GVRAnimation;
+import org.gearvrf.animation.GVRAnimationEngine;
+import org.gearvrf.animation.GVRPositionAnimation;
+import org.gearvrf.animation.GVRRepeatMode;
+import org.gearvrf.scene_objects.GVRSphereSceneObject;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 
-import org.gearvrf.GVRAndroidResource;
-import org.gearvrf.GVRContext;
-import org.gearvrf.GVRHybridObject;
-import org.gearvrf.GVRMaterial;
-import org.gearvrf.GVRSceneObject;
-import org.gearvrf.GVRMaterial.GVRShaderType;
-import org.gearvrf.GVRScene;
-import org.gearvrf.GVRMain;
-import org.gearvrf.GVRTexture;
-import org.gearvrf.GVRTransform;
-import org.gearvrf.animation.GVRAnimation;
-import org.gearvrf.animation.GVRAnimationEngine;
-import org.gearvrf.animation.GVRPositionAnimation;
-import org.gearvrf.animation.GVRRepeatMode;
-import org.gearvrf.animation.GVRTransformAnimation;
-import org.gearvrf.scene_objects.GVRSphereSceneObject;
-
 public class LODTestMain extends GVRMain {
-    
+
     private List<GVRAnimation> animations = new ArrayList<GVRAnimation>();
     private GVRAnimationEngine animationEngine;
-    
+
     @Override
     public void onInit(GVRContext gvrContext) throws IOException {
         animationEngine = gvrContext.getAnimationEngine();
-        
+
         GVRScene scene = gvrContext.getNextMainScene(new Runnable() {
             @Override
             public void run() {
@@ -39,49 +37,52 @@ public class LODTestMain extends GVRMain {
                 }
             }
         });
-        
+
         scene.setFrustumCulling(true);
-        
+
         Future<GVRTexture> redFutureTexture = gvrContext.loadFutureTexture(new GVRAndroidResource(gvrContext, R.drawable.red));
         Future<GVRTexture> greenFutureTexture = gvrContext.loadFutureTexture(new GVRAndroidResource(gvrContext, R.drawable.green));
         Future<GVRTexture> blueFutureTexture = gvrContext.loadFutureTexture(new GVRAndroidResource(gvrContext, R.drawable.blue));
 
+        GVRSphereSceneObject root = new GVRSphereSceneObject(gvrContext);
+
         GVRSphereSceneObject sphereHighDensity = new GVRSphereSceneObject(gvrContext);
+        sphereHighDensity.setName("sphereHighDensity");
         setupObject(gvrContext, sphereHighDensity, redFutureTexture);
-        sphereHighDensity.setLODRange(0.0f, 5.0f);
-        scene.addSceneObject(sphereHighDensity);
-        
+        root.addChildObject(sphereHighDensity);
+
         GVRSphereSceneObject sphereMediumDensity = new GVRSphereSceneObject(gvrContext, 9, 9,
                 true, new GVRMaterial(gvrContext));
-        setupObject(gvrContext, sphereMediumDensity, greenFutureTexture); 
-        sphereMediumDensity.setLODRange(5.0f, 9.0f);
-        scene.addSceneObject(sphereMediumDensity);
-        
+        sphereMediumDensity.setName("sphereMediumDensity");
+        setupObject(gvrContext, sphereMediumDensity, greenFutureTexture);
+        root.addChildObject(sphereMediumDensity);
+
         GVRSphereSceneObject sphereLowDensity = new GVRSphereSceneObject(gvrContext, 6, 6,
                 true, new GVRMaterial(gvrContext));
-        setupObject(gvrContext, sphereLowDensity, blueFutureTexture);   
-        sphereLowDensity.setLODRange(9.0f, Float.MAX_VALUE);
-        scene.addSceneObject(sphereLowDensity);
-        
+        sphereLowDensity.setName("sphereLowDensity");
+        setupObject(gvrContext, sphereLowDensity, blueFutureTexture);
+        root.addChildObject(sphereLowDensity);
+
+        final GVRLODGroup lodGroup = new GVRLODGroup(gvrContext);
+        lodGroup.addRange(9, sphereLowDensity);
+        lodGroup.addRange(0, sphereHighDensity);
+        root.attachComponent(lodGroup);
+        lodGroup.addRange(5, sphereMediumDensity);
+
+        scene.addSceneObject(root);
     }
-    
+
     private void setupObject(GVRContext gvrContext, GVRSceneObject object, Future<GVRTexture> futureTexture) {
         object.getTransform().setPosition(0,  0,  -3.0f);
         GVRMaterial unlit = new GVRMaterial(gvrContext, GVRShaderType.Texture.ID);
         unlit.setMainTexture(futureTexture);
         object.getRenderData().setMaterial(unlit);
-        setupAnimation(object);  
+        setupAnimation(object);
     }
-    
+
     private void setupAnimation(GVRSceneObject object) {
         GVRAnimation animation = new GVRPositionAnimation(object, 2.0f, 0.0f, 0.0f, -10.0f);
         animation.setRepeatMode(GVRRepeatMode.PINGPONG).setRepeatCount(-1);
         animations.add(animation);
     }
-    
-    @Override
-    public void onStep() {
-    }
-    
 }
-
