9909d67dfd0c1fa8c0459a9b9f8d087e10522b39
nola.donato@samsung.com <nola.donato@samsung.com>
2017-02-13 14:04:20 -0800

    subclass GVRAndroidResource in custom IO test
    remove unused imports

--- asset-tests/app/src/androidTest/java/org/gearvrf/tester/AssetImportTests.java
+++ asset-tests/app/src/androidTest/java/org/gearvrf/tester/AssetImportTests.java
@@ -1,5 +1,6 @@
 package org.gearvrf.tester;
 
+
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import net.jodah.concurrentunit.Waiter;
@@ -9,23 +10,21 @@ import org.gearvrf.GVRContext;
 import org.gearvrf.GVRExternalScene;
 import org.gearvrf.GVRScene;
 import org.gearvrf.GVRSceneObject;
-import org.gearvrf.GVRTexture;
-import org.gearvrf.IErrorEvents;
+import org.gearvrf.GVRResourceVolume;
+import org.gearvrf.GVRImportSettings;
 import org.gearvrf.scene_objects.GVRCubeSceneObject;
 import org.gearvrf.scene_objects.GVRModelSceneObject;
 import org.gearvrf.GVRPhongShader;
-import org.gearvrf.IAssetEvents;
 
 import org.gearvrf.unittestutils.GVRTestUtils;
 import org.gearvrf.unittestutils.GVRTestableActivity;
-import org.gearvrf.utility.FileNameUtils;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeoutException;
 
 @RunWith(AndroidJUnit4.class)
@@ -122,6 +121,57 @@ public class AssetImportTests
         mWaiter.assertNotNull(scene.getSceneObjectByName("astro_boy.dae"));
         mTestUtils.waitForXFrames(2);
         mTestUtils.screenShot("AssetImportTests", "canLoadModelWithHandler", mWaiter, mDoCompare);
+    }
+
+    @Test
+    public void canLoadModelWithCustomIO() throws TimeoutException
+    {
+        class ResourceLoader extends GVRResourceVolume
+        {
+            class Resource extends GVRAndroidResource
+            {
+                public Resource(GVRContext ctx, String path) throws IOException
+                {
+                    super(ctx, path);
+                }
+
+                public synchronized void openStream() throws IOException
+                {
+                    // do some special stuff here
+                    super.openStream();
+                }
+            }
+            public int ResourcesLoaded = 0;
+
+            public ResourceLoader(GVRContext ctx, String fileName)
+            {
+                super(ctx, fileName);
+            }
+
+            public GVRAndroidResource openResource(String filePath) throws IOException
+            {
+                ++ResourcesLoaded;
+                Resource resource = new Resource(gvrContext, getFullPath(defaultPath, filePath));
+                return super.addResource(resource);
+            }
+        };
+        GVRContext ctx  = mTestUtils.getGvrContext();
+        GVRScene scene = mTestUtils.getMainScene();
+        GVRModelSceneObject model = new GVRModelSceneObject(ctx);
+        ResourceLoader volume = new ResourceLoader(ctx, "jassimp/astro_boy.dae");
+
+        ctx.getAssetLoader().loadModel(volume, model, GVRImportSettings.getRecommendedSettings(), false, mHandler);
+        mTestUtils.waitForAssetLoad();
+        mWaiter.assertEquals(8, volume.ResourcesLoaded);
+        mHandler.checkAssetLoaded(mWaiter, null, 4);
+        mWaiter.assertNull(scene.getSceneObjectByName("astro_boy.dae"));
+        mWaiter.assertTrue(model.getChildrenCount() > 0);
+        mHandler.checkAssetErrors(mWaiter, 0, 0);
+        mHandler.centerModel(model);
+        scene.addSceneObject(model);
+        mWaiter.assertNotNull(scene.getSceneObjectByName("astro_boy.dae"));
+        mTestUtils.waitForXFrames(2);
+        mTestUtils.screenShot("AssetTests", "canLoadModelWithCustomIO", mWaiter, false);
     }
 
     @Test
