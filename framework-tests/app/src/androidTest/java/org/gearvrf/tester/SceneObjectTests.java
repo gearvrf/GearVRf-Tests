ed63c573eeb45effec24a7d6964d27dd6967b30c
Mihail Marinov <m.marinov@samsung.com>
2017-09-08 08:19:57 -0700

    SceneObjectTests: add a billboard scale test and some cleanup

--- framework-tests/app/src/androidTest/java/org/gearvrf/tester/SceneObjectTests.java
+++ framework-tests/app/src/androidTest/java/org/gearvrf/tester/SceneObjectTests.java
@@ -8,37 +8,33 @@ import net.jodah.concurrentunit.Waiter;
 import org.gearvrf.GVRAndroidResource;
 import org.gearvrf.GVRBillboard;
 import org.gearvrf.GVRContext;
-import org.gearvrf.GVRDirectLight;
 import org.gearvrf.GVRMaterial;
-import org.gearvrf.GVRPointLight;
+import org.gearvrf.GVRPhongShader;
 import org.gearvrf.GVRScene;
 import org.gearvrf.GVRSceneObject;
-import org.gearvrf.GVRSpotLight;
 import org.gearvrf.GVRTexture;
-import org.gearvrf.IAssetEvents;
+import org.gearvrf.GVRTransform;
 import org.gearvrf.scene_objects.GVRCubeSceneObject;
 import org.gearvrf.scene_objects.GVRCylinderSceneObject;
 import org.gearvrf.scene_objects.GVRSphereSceneObject;
-import org.gearvrf.GVRPhongShader;
-import org.gearvrf.unittestutils.GVRTestUtils;
-import org.gearvrf.unittestutils.GVRTestableActivity;
-
 import org.gearvrf.scene_objects.GVRTextViewSceneObject;
 import org.gearvrf.unittestutils.GVRTestUtils;
 import org.gearvrf.unittestutils.GVRTestableActivity;
 import org.gearvrf.utility.Log;
 import org.joml.Vector3f;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
-import java.io.FileNotFoundException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeoutException;
 
 import static java.lang.Thread.sleep;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(AndroidJUnit4.class)
 public class SceneObjectTests
 {
@@ -46,9 +42,7 @@ public class SceneObjectTests
     private GVRTestUtils mTestUtils;
     private Waiter mWaiter;
     private GVRSceneObject mRoot;
-    private GVRSceneObject mBackground;
     private GVRMaterial mBlueMtl;
-    private GVRMaterial mCubeMapMtl;
     private boolean mDoCompare = true;
 
     @Rule
@@ -63,7 +57,7 @@ public class SceneObjectTests
     };
 
     @Before
-    public void setUp() throws TimeoutException
+    public void setUp() throws Exception
     {
         GVRTestableActivity activity = ActivityRule.getActivity();
         mTestUtils = new GVRTestUtils(activity);
@@ -72,25 +66,12 @@ public class SceneObjectTests
         GVRContext ctx  = mTestUtils.getGvrContext();
         GVRScene scene = mTestUtils.getMainScene();
         mWaiter.assertNotNull(scene);
-        Future<GVRTexture> tex = ctx.getAssetLoader().loadFutureCubemapTexture(new GVRAndroidResource(ctx, R.raw.beach));
-        mBlueMtl = new GVRMaterial(ctx, GVRMaterial.GVRShaderType.BeingGenerated.ID);
-        mCubeMapMtl = new GVRMaterial(ctx, GVRMaterial.GVRShaderType.Cubemap.ID);
 
-        mBackground = new GVRCubeSceneObject(ctx, false);
-        mBackground.getTransform().setScale(10, 10, 10);
-        mBackground.getRenderData().setShaderTemplate(GVRPhongShader.class);
-        mBackground.setName("background");
+        mBlueMtl = new GVRMaterial(ctx, GVRMaterial.GVRShaderType.BeingGenerated.ID);
+
         mBlueMtl.setDiffuseColor(0, 0, 1, 1);
-        try
-        {
-            waitForTexture(tex);
-        }
-        catch (InterruptedException ex)
-        {
-            mWaiter.fail(ex);
-        }
-        mCubeMapMtl.setMainTexture(tex);
         mRoot = scene.getRoot();
+
         mWaiter.assertNotNull(mRoot);
     }
 
@@ -122,7 +103,13 @@ public class SceneObjectTests
 
         text.getTransform().setPosition(-1, 0, -2);
         sphere.getTransform().setPosition(1, 0, -2);
-        scene.addSceneObject(mBackground);
+
+        final GVRSceneObject background = new GVRCubeSceneObject(ctx, false);
+        background.getTransform().setScale(10, 10, 10);
+        background.getRenderData().setShaderTemplate(GVRPhongShader.class);
+        background.setName("background");
+        scene.addSceneObject(background);
+
         mRoot.addChildObject(sphere);
         mRoot.addChildObject(text);
         text.setName("text");
@@ -136,12 +123,18 @@ public class SceneObjectTests
     }
 
     @Test
-    public void canDisplaySpheres() throws TimeoutException
+    public void canDisplaySpheres() throws Exception
     {
         GVRContext ctx  = mTestUtils.getGvrContext();
         GVRScene scene = mTestUtils.getMainScene();
         GVRSceneObject sphere1 = new GVRSphereSceneObject(ctx, true, mBlueMtl);
-        GVRSceneObject sphere2 = new GVRSphereSceneObject(ctx, false, mCubeMapMtl);
+
+        final Future<GVRTexture> tex = ctx.getAssetLoader().loadFutureCubemapTexture(new GVRAndroidResource(ctx, R.raw.beach));
+        final GVRMaterial cubeMapMtl = new GVRMaterial(ctx, GVRMaterial.GVRShaderType.Cubemap.ID);
+        tex.get();
+        cubeMapMtl.setMainTexture(tex);
+
+        GVRSceneObject sphere2 = new GVRSphereSceneObject(ctx, false, cubeMapMtl);
 
         sphere1.getRenderData().setShaderTemplate(GVRPhongShader.class);
         sphere1.getTransform().setPosition(0, 0, -4);
@@ -158,12 +151,17 @@ public class SceneObjectTests
     }
 
     @Test
-    public void canDisplayCubes() throws TimeoutException
+    public void canDisplayCubes() throws Exception
     {
         GVRContext ctx  = mTestUtils.getGvrContext();
         GVRScene scene = mTestUtils.getMainScene();
         GVRSceneObject cube1 = new GVRCubeSceneObject(ctx, true, mBlueMtl);
-        GVRSceneObject cube2 = new GVRCubeSceneObject(ctx, false, mCubeMapMtl);
+
+        final Future<GVRTexture> tex = ctx.getAssetLoader().loadFutureCubemapTexture(new GVRAndroidResource(ctx, R.raw.beach));
+        final GVRMaterial cubeMapMtl = new GVRMaterial(ctx, GVRMaterial.GVRShaderType.Cubemap.ID);
+        tex.get();
+        cubeMapMtl.setMainTexture(tex);
+        GVRSceneObject cube2 = new GVRCubeSceneObject(ctx, false, cubeMapMtl);
 
         cube1.getRenderData().setShaderTemplate(GVRPhongShader.class);
         cube1.getTransform().setPosition(0, 0, -4);
@@ -279,6 +277,57 @@ public class SceneObjectTests
         mTestUtils.screenShot(getClass().getSimpleName(), "testBillboardsCamOffset", mWaiter, mDoCompare);
     }
 
+    @Test
+    public void testBillboardOwnersScale() throws TimeoutException {
 
+        GVRContext ctx = mTestUtils.getGvrContext();
+        GVRScene scene = mTestUtils.getMainScene();
+
+        GVRTexture tex = ctx.getAssetLoader().loadTexture(new GVRAndroidResource(ctx, R.drawable.colortex));
+
+        GVRSceneObject quadObj1 = new GVRSceneObject(ctx, 1.0f, 1.0f, tex);
+        quadObj1.setName("quadObj1");
+        quadObj1.getTransform().setPosition(0.8f, 0, -2);
+        quadObj1.getTransform().setRotationByAxis(30, 1,1,1);
+        quadObj1.attachComponent(new GVRBillboard(ctx));
+
+        GVRSceneObject quadObj2 = new GVRSceneObject(ctx, 1.0f, 1.0f, tex);
+        quadObj2.setName("quadObj2");
+        quadObj2.getTransform().setPosition(-1.0f, 0.8f, -2);
+        quadObj2.getTransform().setRotationByAxis(-45, 0,1,0);
+        quadObj2.attachComponent(new GVRBillboard(ctx, new Vector3f(0, 1, 0)));
+
+        GVRSceneObject quadObj3 = new GVRSceneObject(ctx, 1.1f, 1.1f, tex);
+        quadObj3.setName("quadObj3");
+        quadObj3.getTransform().setPosition(-0.5f, -0.8f, -1.4f);
+        quadObj3.getTransform().setRotationByAxis(-45, 1,0,0);
+        quadObj3.attachComponent(new GVRBillboard(ctx, new Vector3f(0, 1, -1)));
+
+        quadObj1.getTransform().setScale(5,6,7);
+        quadObj2.getTransform().setScale(0.5f,0.6f,0.7f);
+        quadObj3.getTransform().setScale(1,1,1);
+
+        scene.addSceneObject(quadObj1);
+        scene.addSceneObject(quadObj2);
+        scene.addSceneObject(quadObj3);
+
+        mTestUtils.waitForXFrames(10);
+
+        final float epsilon = 0.00001f;
+        GVRTransform t = quadObj1.getTransform();
+        mWaiter.assertTrue(Math.abs(5 - t.getScaleX()) < epsilon);
+        mWaiter.assertTrue(Math.abs(6 - t.getScaleY()) < epsilon);
+        mWaiter.assertTrue(Math.abs(7 - t.getScaleZ()) < epsilon);
+
+        t = quadObj2.getTransform();
+        mWaiter.assertTrue(Math.abs(0.5f - t.getScaleX()) < epsilon);
+        mWaiter.assertTrue(Math.abs(0.6f - t.getScaleY()) < epsilon);
+        mWaiter.assertTrue(Math.abs(0.7f - t.getScaleZ()) < epsilon);
+
+        t = quadObj3.getTransform();
+        mWaiter.assertTrue(Math.abs(1 - t.getScaleX()) < epsilon);
+        mWaiter.assertTrue(Math.abs(1 - t.getScaleY()) < epsilon);
+        mWaiter.assertTrue(Math.abs(1 - t.getScaleZ()) < epsilon);
+   }
 
 }
