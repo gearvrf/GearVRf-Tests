b0f48c6def2e1acf5f521f393b040ba8fe9422b8
thomasflynn <tom.flynn@samsung.com>
2018-01-17 08:40:42 -0800

    Astc transparency tests (#224)
    * Add tests for astc transparency.
    
    added the tests to TextureTests since TextureTransparencyTests were
    giving me an error.
    
    * put the astc transparency tests into TextureTransparencyTests.
    
    put the astc transparency tests into TextureTransparencyTests.

--- framework-tests/app/src/androidTest/java/org/gearvrf/tester/TextureTransparencyTests.java
+++ framework-tests/app/src/androidTest/java/org/gearvrf/tester/TextureTransparencyTests.java
@@ -451,5 +451,62 @@ public class TextureTransparencyTests
         android.util.Log.d("Texture:", "rgb8 order = " + order);
         checkResults(order, GVRRenderData.GVRRenderingOrder.GEOMETRY);
     }
+
+    @Test
+    public void testCompressedTextureASTC_Transparent() throws TimeoutException
+    {
+        final GVRContext ctx  = mTestUtils.getGvrContext();
+        final GVRMaterial material = new GVRMaterial(ctx, GVRMaterial.GVRShaderType.Phong.ID);
+        final GVRSceneObject groundObject = new GVRCubeSceneObject(ctx, true, material);
+        TextureEventHandler texHandler = new TextureEventHandler(mTestUtils, 1);
+
+        ctx.getEventReceiver().addListener(texHandler);
+        groundObject.getTransform().setPositionZ(-3.0f);
+        mTestUtils.getMainScene().addSceneObject(groundObject);
+
+        // load astc, RenderOrder == TRANSPARENT
+        try {
+            GVRTexture tex = ctx.getAssetLoader().loadTexture(new GVRAndroidResource(ctx, "3dgreen_transparent.astc"));
+            material.setTexture("diffuseTexture", tex);
+        } catch (IOException ex) {
+            mWaiter.fail(ex);
+        }
+
+        mTestUtils.waitForAssetLoad();
+        texHandler.checkTextureLoaded(mWaiter);
+        mTestUtils.waitForXFrames(2);
+        int order = groundObject.getRenderData().getRenderingOrder();
+        android.util.Log.d("gvrf", "astc transparent order = " + order);
+        checkResults(order, GVRRenderData.GVRRenderingOrder.TRANSPARENT);
+    }
+
+    @Test
+    public void testCompressedTextureASTC_Opaque() throws TimeoutException
+    {
+        final GVRContext ctx  = mTestUtils.getGvrContext();
+        final GVRMaterial material = new GVRMaterial(ctx, GVRMaterial.GVRShaderType.Phong.ID);
+        final GVRSceneObject groundObject = new GVRCubeSceneObject(ctx, true, material);
+        TextureEventHandler texHandler = new TextureEventHandler(mTestUtils, 1);
+
+        ctx.getEventReceiver().addListener(texHandler);
+        groundObject.getTransform().setPositionZ(-3.0f);
+        mTestUtils.getMainScene().addSceneObject(groundObject);
+
+        // load astc, RenderOrder == GEOMETRY
+        texHandler.reset();
+        try {
+            GVRTexture tex = ctx.getAssetLoader().loadTexture(new GVRAndroidResource(ctx, "3dgreen_opaque.astc"));
+            material.setTexture("diffuseTexture", tex);
+        } catch (IOException ex) {
+            mWaiter.fail(ex);
+        }
+
+        mTestUtils.waitForAssetLoad();
+        texHandler.checkTextureLoaded(mWaiter);
+        mTestUtils.waitForXFrames(2);
+        int order = groundObject.getRenderData().getRenderingOrder();
+        android.util.Log.d("Texture:", "astc opaque order = " + order);
+        checkResults(order, GVRRenderData.GVRRenderingOrder.GEOMETRY);
+    }
 }
 
