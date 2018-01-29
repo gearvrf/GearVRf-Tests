14eeb1149ae73be613fc5f0e613dcef9f76513a9
Eliamara Silva <eliamara.s@samsung.com>
2018-01-29 18:09:55 -0400

    Add base classes to parse json
    * Add GVRSceneMaker class to create a scene from a json string
    * Add RGBAColor class to represent a rgba color.
    
    GearVRf-DCO-1.0-Signed-off-by: Eliamara Silva <eliamara.s@samsung.com>

---
+++ gvr-unittestutils/src/main/java/org/gearvrf/unittestutils/GVRSceneMaker.java
@@ -0,0 +1,17 @@
+package org.gearvrf.unittestutils;
+
+public class GVRSceneMaker {
+    private static class RGBAColor {
+        public final float r;
+        public final float g;
+        public final float b;
+        public final float a;
+
+        public RGBAColor(float r, float g, float b, float a){
+            this. r = r;
+            this. g = g;
+            this. b = b;
+            this. a = a;
+        }
+    }
+}
