e16ee4e985ac2459b0f30017f5191e3a183a484b
Ragner Magalhaes <ragner.n@samsung.com>
2018-01-29 18:13:08 -0400

    Parse json array to array of int or float
    * Add some convenience function to parse json arrays
    of int and float
    
    GearVRf-DCO-1.0-Signed-off-by: Ragner Magalhaes <ragner.n@samsung.com>

--- gvr-unittestutils/src/main/java/org/gearvrf/unittestutils/GVRSceneMaker.java
+++ gvr-unittestutils/src/main/java/org/gearvrf/unittestutils/GVRSceneMaker.java
@@ -1,5 +1,8 @@
 package org.gearvrf.unittestutils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 public class GVRSceneMaker {
     private static class RGBAColor {
         public final float r;
@@ -13,5 +16,25 @@ public class GVRSceneMaker {
             this. b = b;
             this. a = a;
         }
+    }
+
+    private static float[] jsonToFloatArray(JSONArray jsonArray) throws JSONException {
+        float[] array = new float[jsonArray.length()];
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            array[i] = (float) jsonArray.getDouble(i);
+        }
+
+        return array;
+    }
+
+    private static int[] jsonToIntArray(JSONArray jsonArray) throws JSONException {
+        int[] array = new int[jsonArray.length()];
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            array[i] = jsonArray.getInt(i);
+        }
+
+        return array;
     }
 }
