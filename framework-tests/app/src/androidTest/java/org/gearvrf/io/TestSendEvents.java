ed5e02f216a7953409599f4337dff0d411e4f3b4
Nola Donato <nola.donato@samsung.com>
2018-02-21 07:49:49 -0800

    Fix crash in picker tests (#248)
    * Fix crash in picker tests
    
    The pick handler used to fail if it got an onExit without a corresponding onEnter. This happens sometimes because the scene is cleared before the test completely exits. This patch changes the pick handler to ignore spurious onExit calls.
    
    Also removed redundant version of RenderPerfTests

--- framework-tests/app/src/androidTest/java/org/gearvrf/io/TestSendEvents.java
+++ framework-tests/app/src/androidTest/java/org/gearvrf/io/TestSendEvents.java
@@ -44,7 +44,7 @@ public class TestSendEvents {
         };
         Looper.myLooper().quit();
 
-        final SendEvents sendEvents = new SendEvents(dummyActivity);
+        final GVRGearCursorController.SendEvents sendEvents = new GVRGearCursorController.SendEvents(dummyActivity);
 
         final Runnable producer = new Runnable() {
             @Override
